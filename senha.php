a4c8dff8b26bd87053d8a92c04bc4378a6e8e9fc
Tatubolinha666 <88353359+Tatubolinha666@users.noreply.github.com>
2021-09-22 23:29:17 -0300

    Add files via upload

--- senha.php
+++ senha.php
@@ -1,16 +1,16 @@
 <?php 
-echo "Cadastre sua senha:";
+session_start();
+echo "<br/>Cadastre sua senha:";
 $senha=123456;
-echo "Digite um número real:";
+echo "<br/>Digite um número real:";
 $n1=15;
-echo "Digite outro número real";
+echo "<br/>Digite outro número real";
 $n2=3;
 $div=$n1/$n2;
-echo "Digite novamente sua senha: ";
-if (!preg_match('/^[123456]*/',$senha)) {
-	echo "O resultado da divisão é igual a: ",$div;
+echo "<br/>Digite novamente sua senha: ";
+if ($senha) {
+	echo "<br/>O resultado da divisão é igual a: ",$div;
 }else
 echo "Erro";
 session_destroy();
-header("Location: index.php");
  ?>
