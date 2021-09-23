fea80260de84388727f2f99a930ed325b2d094d6
Tatubolinha666 <88353359+Tatubolinha666@users.noreply.github.com>
2021-09-22 22:44:43 -0300

    Add files via upload

--- salariocontas.php
+++ salariocontas.php
@@ -2,18 +2,26 @@
 $salariohora=75;
 $qtdhoras=55;
 $dependentes=3;
-
 $salariobruto=$salariohora*$qtdhoras+(50*$dependentes);
+$ir=$salariobruto*5/100;
+$ir1=$salariobruto*7/100;
 if ($salariobruto<=1000) {
  
-		echo "INSS desconto será igual a: ".$salariobruto*8.5/100;
+		echo "INSS desconto: ".$salariobruto*8.5/100," reais";
 		
 		 } else  {
-		echo "Desconto INSS será de:
-		".$salariobruto*9/100;
-	
+		echo "Desconto INSS:
+		".$salariobruto*9/100," reais";
 	}
 
+if  ( $salariobruto <=500 ) { 
+	echo "<br/>Sem desconto em Imposto de Renda.";
+	}if ($salariobruto<=1000) {
+	 	echo "<br/>Desconto Imposto de Renda: ".$ir." reais";
+	 }  else {
+		echo "<br/>Desconto Imposto de Renda: ".$ir1." reais";
+	}
+	
 
 
 
